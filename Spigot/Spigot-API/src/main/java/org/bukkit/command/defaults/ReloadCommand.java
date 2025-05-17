4af8d4cae2ce3f509d6d03baa7bed8cf758fe7a0
Rafael Mendes Siqueira <rms-aps@hotmail.com>
2025-05-17 10:24:45 -0300

    Spigot-1.8.8-EnderChestPlus

---
+++ Spigot/Spigot-API/src/main/java/org/bukkit/command/defaults/ReloadCommand.java
@@ -0,0 +1,38 @@
+package org.bukkit.command.defaults;
+
+import java.util.Arrays;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+
+public class ReloadCommand extends BukkitCommand {
+    public ReloadCommand(String name) {
+        super(name);
+        this.description = "Recarrega a configuração do servidor e plugins";
+        this.usageMessage = "/reload";
+        this.setPermission("bukkit.command.reload");
+        this.setAliases(Arrays.asList("rl"));
+    }
+
+    @Override
+    public boolean execute(CommandSender sender, String currentAlias, String[] args) {
+        if (!testPermission(sender)) return true;
+
+        Command.broadcastCommandMessage(sender, ChatColor.RED + "Esse comando pode ocorrer bugs nos seus plugins.");
+        Command.broadcastCommandMessage(sender, ChatColor.RED + "Qualquer erro use /stop...");
+        Bukkit.reload();
+        Command.broadcastCommandMessage(sender, ChatColor.GREEN + "Reload completado.");
+
+        return true;
+    }
+
+    // Spigot Start
+    @Override
+    public java.util.List<String> tabComplete(CommandSender sender, String alias, String[] args) throws IllegalArgumentException
+    {
+        return java.util.Collections.emptyList();
+    }
+    // Spigot End
+}
